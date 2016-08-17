3f5dd6095856c8b8159ef0c8a85717eda4b91eec
Vinicius Pacheco <vin175pacheco@gmail.com>
2016-08-17 19:09:33 -0300

    Add container and breakpoints + Removed containers.scss

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,7 +7,7 @@ export default class App extends Component {
         <header className="header">
           <div className="container">
             <header className="navbar-default navbar">
-              <a href="" className="navbar-brand">Starter Kit</a>
+              <a href="" className="navbar-brand">Starter kit</a>
               <ul className="nav navbar-nav">
                 <li><a href="" className="active">Features</a></li>
                 <li><a href="">Pricing</a></li>
@@ -16,11 +16,61 @@ export default class App extends Component {
               <ul className="nav navbar-nav right">
                 <li><a href="">Documentation</a></li>
                 <li><a href="">Support</a></li>
-                <li><button className="btn btn-outline-primary navbar-btn">Entrar</button></li>
               </ul>
             </header>
           </div>
         </header>
+        <div className="container section-padding">
+          <div className="row">
+            <div className="col-6">
+              <article className="article">
+                <div className="img-cover" />
+                <div className="article-content">
+                  <img src="https://stripe.com/img/v3/gallery/quote-logos/kickstarter.png" width="182" height="22" alt="Kickstarter"/>
+                  <p className="margin-top-md">Kickstarter switched to Stripe to accelerate their support for international creators. Since partnering with Stripe to navigate international payment methods and compliance, Kickstarter’s phenomenally successful platform for creative projects is now available to creators in 18 countries.</p>
+                </div>
+              </article>
+            </div>
+            <div className="col-6">
+              <div className="article-text-content">
+                <p className="text-lg text-pre-muted">The best platforms own their user experience from end to end, including payments. Purchasing is woven into Twitter and Shopify. Kickstarter and Squarespace get their sellers paid seamlessly. These services work just right.</p>
+                <p className="paragraph text-muted">It only takes a few minutes to start adding commerce to your platform. To manage payments, you can set up Stripe accounts for sellers from 25 countries (more coming soon). Or own the look and feel yourself and have sellers do everything within your platform.</p>
+                <p className="paragraph text-muted">With Stripe, Squarespace went from a site-builder to a store-builder—growing its customer base by becoming more useful to more people. And thousands of other platforms around the world are bringing payments in-house with Stripe.</p>
+                <hr className="hr-muted hr-lg"/>
+                <ul className="nav nav-vertical">
+                  <li><a href="">Bring payments to your platform</a></li>
+                  <li><a href="">Create a ’Buy Now’ button for your app</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-blue section-padding">
+          <div className="container">
+            <div className="row">
+              <div className="col-8 col-offset-2">
+                <p className="text-lg text-pre-muted">The on-demand economy gives the people what they want, now: rides, food, accommodation, and more. Your payments should meet the same high standards as your product experience: with just a tap, Stripe helps sellers on your marketplace get paid.</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <article className="article-2 margin-top margin-bottom-lg"></article>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-5">
+                <p className="parafgraph text-muted">Payments for <span>market­places</span> used to be hard: accounting for seller earnings, making en-masse payouts happen (on time!), managing 1099-Ks and tax reporting, regulatory compliance. Marketplaces shouldn’t have to build all that from scratch, so we made Stripe Connect to provide all the tools you need to run a multi-sided marketplace.</p>
+              </div>
+              <div className="col-5 col-offset-2">
+                <p className="paragraph text-muted">Integrating Connect means always having access to the state-of-the-art. In October 2015, we started piloting <a href="">instant debit card transfers</a>. And Connect is now available to marketplaces in <a href="">25 countries</a>.</p>
+                <hr className="hr-muted hr-lg"/>
+                <ul className="nav nav-vertical">
+                  <li><a href="">Read more about Connect</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
 import configureStore from './redux/configureStore'
 import App from './components/App'
+import { Provider } from 'react-redux'
 import './dist/css/app.css'
 
 const store = configureStore();
